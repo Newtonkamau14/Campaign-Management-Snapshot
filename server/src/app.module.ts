cf5480b13fbee23759eeb967fe3827ef8b485dc8
Newtonkamau14 <newtonkamau14@gmail.com>
2025-02-11 16:01:03 +0300

    feat: add spec file for app,chore: add imports for various controllers,modules

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,16 +1,38 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from "@nestjs/mongoose";
+import { MongooseModule } from '@nestjs/mongoose';
 import { CampaignController } from './campaign/campaign.controller';
 import { CampaignModule } from './campaign/campaign.module';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
 import { AuthModule } from './auth/auth.module';
+import { InfluencerModule } from './influencer/influencer.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { InfluencerController } from './influencer/influencer.controller';
+import { SubmissionModule } from './submission/submission.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(process), CampaignModule, AuthModule],
-  controllers: [AppController, CampaignController, AuthController],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    CampaignModule,
+    AuthModule,
+    InfluencerModule,
+    SubmissionModule,
+  ],
+  controllers: [
+    AppController,
+    CampaignController,
+    AuthController,
+    InfluencerController,
+  ],
   providers: [AppService, AuthService],
 })
 export class AppModule {}
