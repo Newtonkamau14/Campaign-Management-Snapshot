932993796a4b5309df5b47da9044a49d35671e35
Newtonkamau14 <newtonkamau14@gmail.com>
2025-02-11 16:01:38 +0300

    chore: change location of files

--- server/src/campaign/schemas/instruction.schema.ts
+++
@@ -1,9 +0,0 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-
-@Schema()
-export class Instruction {
-  @Prop({ required: true, type: String })
-  instruction: string;
-}
-
-export const InstructionSchema = SchemaFactory.createForClass(Instruction);
